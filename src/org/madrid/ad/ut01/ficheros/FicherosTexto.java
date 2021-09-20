088e9b58b6ff840bf85ae3db91e2975c36ca597a
oscar <trissaikyt.g@gmail.com>
2021-09-20 18:37:41 +0200

    Ej6

--- src/org/madrid/ad/ut01/ficheros/FicherosTexto.java
+++ src/org/madrid/ad/ut01/ficheros/FicherosTexto.java
@@ -1,5 +1,6 @@
 package org.madrid.ad.ut01.ficheros;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -24,25 +25,8 @@ public class FicherosTexto implements InterfazFicherosTexto {
 
 	@Override
 	public int contarCaracteres(String rutaFichero) {
-		int cuenta=0;
-		File fichero = new File(rutaFichero);
-		try (FileReader fr = new FileReader(fichero)) {
-			if (!fichero.exists()) {
-				System.out.println("El archivo no existe.");
-				return 0;
-			}
-			
-			while (true) {
-				int letra = fr.read();
-				if(letra==-1) break;
-				if (Character.isAlphabetic(letra)) cuenta++;
 
-			}
-		} catch (IOException e) {
-
-			e.printStackTrace();
-		}
-		return cuenta;
+		return 0;
 	}
 
 	@Override
@@ -53,8 +37,31 @@ public class FicherosTexto implements InterfazFicherosTexto {
 
 	@Override
 	public int contarPalabras(String rutaFichero) {
-		// TODO Auto-generated method stub
-		return 0;
+		File file = new File(rutaFichero);
+		String linea;
+		int num=0;
+		try(BufferedReader fr= new BufferedReader(new FileReader(file))) {
+			if(!file.exists()) {
+				System.out.println("El fichero no existe"); 
+				return 0;
+			}
+			while(true) {
+			linea=fr.readLine();
+			if(linea==null) return num;
+			String[]lineaAux=linea.split(" ");
+			for(String palabra: lineaAux) {
+				num++;
+			}
+				
+			
+			}
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return num;
 	}
 
 	@Override
--- src/org/madrid/ad/ut01/ficheros/Main.java
+++ src/org/madrid/ad/ut01/ficheros/Main.java
@@ -5,9 +5,10 @@ package org.madrid.ad.ut01.ficheros;
 public class Main {
 	
 	public static void main(String[] args) {
+		String ruta = "src/org/madrid/ad/ut01/ficheros/assets/el_quijote.txt";
 			FicherosTexto ejercicios = new FicherosTexto();
-			ejercicios.leer("src/org/madrid/ad/ut01/ficheros/assets/el_quijote.txt");
-		
+//			System.out.println(ejercicios.contarCaracteres(ruta));
+			System.out.println(ejercicios.contarPalabras(ruta));
 
 	}
 
