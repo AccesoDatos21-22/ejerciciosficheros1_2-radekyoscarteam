9810d27981f6b1f69b1aadea99fe73c50a31aa05
oscar <trissaikyt.g@gmail.com>
2021-09-20 09:57:58 +0200

    Subida Ejercicio 1

--- src/org/madrid/ad/ut01/ficheros/FicherosTexto.java
+++ src/org/madrid/ad/ut01/ficheros/FicherosTexto.java
@@ -1,21 +1,43 @@
 package org.madrid.ad.ut01.ficheros;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.madrid.ad.ut01.ficheros.interfaces.InterfazFicherosTexto;
 
 /**
  * 
  * @author Escribe_aqui_tu_nombre
- * @date 
- * @version 
+ * @date
+ * @version
  * @license GPLv3
  *
  */
-public class FicherosTexto implements InterfazFicherosTexto{
+public class FicherosTexto implements InterfazFicherosTexto {
 
 	@Override
 	public void leer(String rutaFichero) {
-		// TODO Auto-generated method stub
-		
+
+		File fichero = new File(rutaFichero);
+		try (FileReader fr = new FileReader(fichero)) {
+			if (!fichero.exists()) {
+				System.out.println("El archivo no existe.");
+				return;
+			}
+
+			while (true) {
+				int letra = fr.read();
+				if (letra == -1)  break;  //Si devuelve -1 significa que no quedan caracteres por leer
+				System.out.print((char) letra);
+
+			}
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
@@ -59,7 +81,5 @@ public class FicherosTexto implements InterfazFicherosTexto{
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
-
 
 }
