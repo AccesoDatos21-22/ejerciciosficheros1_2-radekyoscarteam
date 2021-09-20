2fb1d51a7e11a3ac0b899fa7de41cb01d6b48ff9
oscar <trissaikyt.g@gmail.com>
2021-09-20 10:23:31 +0200

    aa

--- src/org/madrid/ad/ut01/ficheros/FicherosTexto.java
+++ src/org/madrid/ad/ut01/ficheros/FicherosTexto.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+
 import org.madrid.ad.ut01.ficheros.interfaces.InterfazFicherosTexto;
 
 /**
@@ -19,31 +20,29 @@ public class FicherosTexto implements InterfazFicherosTexto {
 
 	@Override
 	public void leer(String rutaFichero) {
+	}
 
+	@Override
+	public int contarCaracteres(String rutaFichero) {
+		int cuenta=0;
 		File fichero = new File(rutaFichero);
 		try (FileReader fr = new FileReader(fichero)) {
 			if (!fichero.exists()) {
 				System.out.println("El archivo no existe.");
-				return;
+				return 0;
 			}
-
+			
 			while (true) {
 				int letra = fr.read();
-				if (letra == -1)  break;  //Si devuelve -1 significa que no quedan caracteres por leer
-				System.out.print((char) letra);
+				if(letra==-1) break;
+				if (Character.isAlphabetic(letra)) cuenta++;
 
 			}
 		} catch (IOException e) {
 
 			e.printStackTrace();
 		}
-
-	}
-
-	@Override
-	public int contarCaracteres(String rutaFichero) {
-		// TODO Auto-generated method stub
-		return 0;
+		return cuenta;
 	}
 
 	@Override
