3d83421ad2245a66cc0fd092a25404050038ad07
oscar <trissaikyt.g@gmail.com>
2021-09-23 13:58:11 +0200

    Main

--- src/org/madrid/ad/ut01/ficheros/Main.java
+++ src/org/madrid/ad/ut01/ficheros/Main.java
@@ -5,16 +5,19 @@ package org.madrid.ad.ut01.ficheros;
 public class Main {
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
-=======
+
 		String ruta = "src/org/madrid/ad/ut01/ficheros/assets/el_quijote.txt";
 			FicherosTexto ejercicios = new FicherosTexto();
-//			System.out.println(ejercicios.contarCaracteres(ruta));
-//			System.out.println(ejercicios.contarPalabras(ruta));
+			System.out.println("Caracteres totales: "+ejercicios.contarCaracteres(ruta));
+			System.out.println("Palabras totales: "+ejercicios.contarPalabras(ruta));
 			
-//			System.out.println(ejercicios.frecuenciaLetras(ruta));
-			ejercicios.frecuenciaLetras(ruta).forEach((k,v)->System.out.println("letra: "+k+", valor: "+ v));
->>>>>>> ejercicio10
+			System.out.println("\nFrecuencia de Vocales: \n");
+			ejercicios.frecuenciaVocales(ruta).forEach((k,v)->System.out.println("Vocal: "+k+", valor: "+ v));
+
+			System.out.println("\nFrecuencia de Letras: |n");
+
+			ejercicios.frecuenciaLetras(ruta).forEach((k,v)->System.out.println("Letra: "+k+", valor: "+ v));
+
 
 	}
 
