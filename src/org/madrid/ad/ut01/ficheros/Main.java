ae831e42d30d07f976b560ecdbf5e5be38186b3e
oscar <trissaikyt.g@gmail.com>
2021-09-23 12:55:55 +0200

    s

--- src/org/madrid/ad/ut01/ficheros/Main.java
+++ src/org/madrid/ad/ut01/ficheros/Main.java
@@ -5,9 +5,6 @@ package org.madrid.ad.ut01.ficheros;
 public class Main {
 	
 	public static void main(String[] args) {
-			FicherosTexto ejercicios = new FicherosTexto();
-			ejercicios.contarCaracteres("src/org/madrid/ad/ut01/ficheros/assets/el_quijote.txt");
-		
 
 	}
 
